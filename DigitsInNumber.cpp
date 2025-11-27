bb512827321037c0a03c8ef2887147a81e9b3460
KrutikaChincholkar07 <chincholkarkrutika07@gmail.com>
2025-11-27 23:47:49 +0530

    Create DigitsInNumber.cpp
    Example:
    Number = 5234
    Steps:
    5234 → 523 → 52 → 5 → 0
    Count = 4 digits

---
+++ DigitsInNumber.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, count = 0, temp;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    temp = num;
+
+    if (temp == 0) {
+        count = 1;
+    } else {
+        while (temp != 0) {
+            temp /= 10;
+            count++;
+        }
+    }
+
+    cout << "Number of digits: " << count;
+
+    return 0;
+}
