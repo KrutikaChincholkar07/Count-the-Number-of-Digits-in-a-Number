cdf689c8868d6295ae4da49a8ba7a183edf96869
KrutikaChincholkar07 <chincholkarkrutika07@gmail.com>
2025-11-27 23:51:50 +0530

    Update DigitsInNumber.c
    Count the Number of Digits in a Number code in C
    Example:
    Number = 5234
    Steps:
    5234 → 523 → 52 → 5 → 0
    Count = 4 digits

--- DigitsInNumber.c
+++ DigitsInNumber.c
@@ -1,6 +1,21 @@
-Count the Number of Digits in a Number code in C
-Example:
-Number = 5234
-Steps:
-5234 → 523 → 52 → 5 → 0
-Count = 4 digits
+#include <stdio.h>
+
+int main() {
+    int num, count = 0, temp;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    if (temp == 0) {
+        count = 1;
+    } else {
+        while (temp != 0) {
+            temp /= 10;
+            count++;
+        }
+    }
+    printf("Number of digits: %d", count);
+    return 0;
+}
